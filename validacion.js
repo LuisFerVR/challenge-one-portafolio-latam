50548c834a701b65df5e5cd774c5997c1f35ce6a
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-04-17 18:28:33 -0600

    Backlogs de Trello concluidos. Falta Más Css

--- validacion.js
+++ validacion.js
@@ -1,1 +1,48 @@
 //Haz tú validación en javascript acá
+function fnGuardarContacto(){
+    let IContacto = [];
+    let nombre = document.getElementById('inputNombre').value;
+    let correo =  document.getElementById("inputCorreo").value;
+    let asunto = document.getElementById("inputAsunto").value;
+    let inputMsj = document.getElementById("inputMsj").value;
+
+    var expemail = new RegExp('^(.+)@(\\S+)$');
+    if(nombre,correo,asunto,inputMsj!=''){
+        if(nombre<=50)
+        {
+            IContacto.push(nombre)
+        
+        }else{
+            alert("Ingresa tu nombre no mayor a 50 caracteres");
+        };
+
+        if(expemail.test(correo))
+        {
+            IContacto.push(nombre)
+        
+        }else{
+            alert("Ingresa un correo válido con @ y ., ejmeplo: luis@gmail.com");
+        };
+
+        if(asunto<=50)
+        {
+            IContacto.push(nombre)
+        
+        }else{
+            alert("Ingresa un asunto no mayor a 50 caracteres");
+        };
+
+        if(inputMsj<=300)
+        {
+            IContacto.push(nombre)
+        
+        }else{
+            alert("Ingresa un mensaje no mayor a 300 caracteres");
+        };
+        
+        let json = {
+            persona:IContacto
+        }
+        console.log(json);
+    }
+}
